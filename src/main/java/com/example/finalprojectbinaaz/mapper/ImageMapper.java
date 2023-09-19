119141dc3699563059578700e95611043f4914d5
AytanGasimova <aytangasimova148@gmail.com>
2023-09-20 00:25:55 +0400

    ImageMapperTest done!

--- src/main/java/com/example/finalprojectbinaaz/mapper/ImageMapper.java
+++ src/main/java/com/example/finalprojectbinaaz/mapper/ImageMapper.java
@@ -6,5 +6,5 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ImageMapper {
-    ImageEntity mapDtoToEntity(ImageDto pimageDto);
+    ImageEntity mapDtoToEntity(ImageDto imageDto);
 }
---
+++ src/test/groovy/com/example/finalprojectbinaaz/mapper/ImageMapperTest.groovy
@@ -0,0 +1,37 @@
+package com.example.finalprojectbinaaz.mapper
+
+import com.example.finalprojectbinaaz.dao.entity.ImageEntity
+import com.example.finalprojectbinaaz.model.ImageDto
+import io.github.benas.randombeans.EnhancedRandomBuilder
+import io.github.benas.randombeans.api.EnhancedRandom
+import spock.lang.Specification
+
+import org.mapstruct.factory.Mappers
+
+class ImageMapperTest extends Specification {
+    private ImageMapper imageMapper
+    private ImageDto imageDto
+    private EnhancedRandom random = EnhancedRandomBuilder.aNewEnhancedRandom()
+
+    void setup() {
+
+    }
+
+
+    def "MapDtoToEntity successes"() {
+        given:
+        imageMapper = Mappers.getMapper(ImageMapper.class)
+
+        imageDto = random.nextObject(ImageDto)
+
+        when:
+        ImageEntity result = imageMapper.mapDtoToEntity(imageDto)
+
+        then:
+        result != null
+
+        and:
+        result.image == imageDto.image
+        result.isPreviewImage() == imageDto.isPreviewImage()
+    }
+}
